120a1de759362561269c382ccbb770569fa51f63
Molcarrus <lenovolaptop.1202023@gmail.com>
2025-09-16 03:54:21 +0530

    main

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ mod leg;
 mod spider;
 mod camera;
 
+use bevy::{prelude::*, window};
+
+use camera::CameraPlugin;
+use kin::IkPlugin;
+use spider::SpiderPlugin;
+use world::WorldPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((DefaultPlugins, CameraPlugin, WorldPlugin, IkPlugin, SpiderPlugin))
+        .run();
 }
